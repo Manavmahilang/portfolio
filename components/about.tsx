770595172b497337fb2dc929ffb1d1c74be3ba46
Manav Mahilang <68985296+Manavmahilang@users.noreply.github.com>
2024-07-19 14:15:10 +0530

    build error cleared

--- components/about.tsx
+++ components/about.tsx
@@ -1,12 +1,12 @@
-"use client";
+'use client';
 
-import React from "react";
-import SectionHeading from "./section-heading";
-import { motion } from "framer-motion";
-import { useSectionInView } from "@/lib/hooks";
+import React from 'react';
+import SectionHeading from './section-heading';
+import { motion } from 'framer-motion';
+import { useSectionInView } from '@/lib/hooks';
 
 export default function About() {
-  const { ref } = useSectionInView("About");
+  const { ref } = useSectionInView('About');
 
   return (
     <motion.section
@@ -19,26 +19,26 @@ export default function About() {
     >
       <SectionHeading>About me</SectionHeading>
       <p className="mb-3">
-        After graduating with a degree in{" "}
-        <span className="font-medium">Computer Science(B.Tech)</span>, I Joined Advertyzment {" "}
-        <span className="font-medium">as a backend developer</span>.{" "}
+        After graduating with a degree in{' '}
+        <span className="font-medium">Computer Science(B.Tech)</span>, I joined Advertyzment{' '}
+        <span className="font-medium">as a backend developer</span>.{' '}
         <span className="italic">My favorite part of programming</span> is the
-        'Eureka' moment which comes after a problem which sometimes pains us for days. I <span className="underline">love</span> the
+        &apos;Eureka&apos; moment which comes after a problem which sometimes pains us for days. I <span className="underline">love</span> the
         feeling of finally figuring out a solution to a problem. My core stack
-        is{" "}
+        is{' '}
         <span className="font-medium">
-          Python, C++ , Javascript, React, Next.js and SQL
+          Python, C++, Javascript, React, Next.js, and SQL
         </span>
-        . I am also familiar with variety of languages and softwares. I am always looking to
-        learn new technologies. I am currently looking for a{" "}
+        . I am also familiar with a variety of languages and software. I am always looking to
+        learn new technologies. I am currently looking for a{' '}
         <span className="font-medium">full-time position</span> as a software
         developer.
       </p>
 
       <p>
-        <span className="italic">When I{'\''}m not coding</span>, I enjoy playing
-        video games, watching movies, and hitting the gym. I also enjoy{" "}
-        <span className="font-medium">learning new things</span>. I{'\''}m also
+        <span className="italic">When I&apos;m not coding</span>, I enjoy playing
+        video games, watching movies, and hitting the gym. I also enjoy{' '}
+        <span className="font-medium">learning new things</span>. I&apos;m also
         learning how to play the guitar.
       </p>
     </motion.section>
